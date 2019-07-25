d520e487ca64d72ebd6ddb24245a455405269e46
VSMent <vsmentvs@gmail.com>
2019-07-25 11:30:25 -0600

    Player draw, general comments

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
+# imports
 import pygame
+
+#variables
+display_width = 800
+display_height = 600
+
+
+black = (0,0,0)
+white = (255,255,255)
+
+#code
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Deep Maze!')
 clock = pygame.time.Clock()
+playerImg = pygame.image.load("sprites/hero/idleA/hero_idleA_0000.png") # w:100 h:110 
 
-crashed = False
+def showPlayer(x,y):
+  gameDisplay.blit(playerImg,(x,y)) # draw image
 
-while not crashed:
+playerX = (display_width * 0.45)
+playerY = (display_height * 0.8)
+
+
+
+isEnd = False
+# main loop
+while not isEnd:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      crashed = True
-    print(event)
+      isEnd = True
+    # print(event) # DEBUG
+  gameDisplay.fill(white)
+  showPlayer(playerX,playerY)
 
   pygame.display.update() # update specific thing if specified or whole screen
   clock.tick(60)
 
+# quit
 pygame.quit()
 quit()
 
